64af9ba6b37219ab97633fc43d586382896b1003
SOT001 <github@sotiroff.eu>
2015-06-29 15:17:45 +0100

    added in switches

--- record_triplej.sh
+++ record_triplej.sh
@@ -1,15 +1,43 @@
 #!/bin/bash
 
-#
-STREAM=$1
-DURATION=`echo ${2}*60 | bc`
-LOCATION=$3
+usage () {
+	echo "Usage: $0 -u stream -t duration -d location" >&2
+}
+
+
+if [ $# -lt 3 ]; then
+	usage
+	exit 1	
+fi
+
+# get arguments
+url=  time= dir=
+
+while getopts u:t:d: opt
+do
+        case $opt in
+        u)      url=$OPTARG
+                ;;
+        t)      time=`echo ${OPTARG}*60 | bc`
+                ;;
+        d)      dir=$OPTARG
+                ;;
+        '?')    echo "$0: invalid option -$OPTARG" >&2
+				usage
+                exit 1
+                ;;
+        esac
+done
+
+shift $((OPTIND -1))
+
 FILE=triplej.mp3
 
-cd $LOCATION
-rm -f triplej*
+cd $dir
+# should have this as an argument too
+rm -f triplej.*
 
-/usr/bin/streamripper $STREAM -A -l $DURATION -a $FILE -d $LOCATION -u "FreeAmp/2.x" --quiet --with-id3v1 
+/usr/bin/streamripper $url -o always -A -l $time -a $FILE -d $dir -u "FreeAmp/2.x" --quiet --with-id3v1 
 
 /usr/bin/id3tool -t "Triple J" -a "Triple J" -r "Various" -y "2015" $FILE
 
